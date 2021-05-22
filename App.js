7b8e21d02c4f00fd2be667a705fe5dba1d592f2e
ElieDahrouj <edarouj@hotmail.com>
2021-05-23 00:53:30 +0200

    add tab navigation

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React,{useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen'
 import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import axios from "axios"
 
-export default function App() {
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
 
     useEffect(() => {
         SplashScreen.hide();
     });
 
     return (
-        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <Text>Hello</Text>
-        </View>
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({color, size }) => {
+                        let iconName;
+                        if (route.name === 'Home') {
+                            iconName = "home";
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: '#FF8C00',
+                    inactiveTintColor: '#707070',
+                }}
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
+export default App;
